5bde36eff0b5677b2b12deda002f28b0632df4af
Anton <anton.mois33v@gmail.com>
2019-02-13 13:56:32 -0500

    Completed exercise 0

---
+++ exercises.py
@@ -0,0 +1,25 @@
+# exercise 0 <Creating LISTS and DICTIONARIES>
+# my lists
+fav_colours = ["green", "teal", "gray", "black"]
+ages_list = [25, 23, 17, 17, 49]
+# if heads -> True, tails -> False
+coin_flips = [True, True, True, False, True]
+fav_artists = ["James Blake", "Post Malone", "Joji"]
+
+# my dictionaries
+words = {
+"implication": "The conclusion that can be drawn from something although it is not explicitly stated.",
+"truth": "That which is true or in accordance with fact or reality.",
+"routine": "A sequence of actions regularly followed; a fixed program."
+}
+fav_movies = {
+"Seven Pounds": 2008,
+"Mad Max: Fury Road": 2015,
+"The Fifth Element": 1997
+}
+cities = {
+"Moscow": 12000000,
+"Beijing": 21500000,
+"Lisbon": 500000
+}
+ages_dict = {"Mia": 23, "Sasha": 17, "Nastja": 17, "Natalya": 49}
